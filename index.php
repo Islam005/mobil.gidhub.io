d3619013746a5251048109d7f69b7723592d2ae6
Islam005 <31561223+Islam005@users.noreply.github.com>
2017-09-02 11:13:36 +0400

    ondec

---
+++ index.php
@@ -0,0 +1,12 @@
+<head>
+<title>Мой сайт</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+<center>
+			<div id="mainhead"><?php include_once "upload/header.php";?></div>
+			<div id="maincontent"><?php include_once "upload/content.php";?></div>
+			<div id="mainfooter"><?php include_once "upload/footer.php";?></div>
+</center>
+</body>
---
+++ upload/header.php
@@ -0,0 +1,14 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+
+<ul>
+<div id="menu"><a href="*">Тарифы</a></div>
+<div id="menu"><a href="*">Услуги</a></div>
+<div id="menu"><a href="*">Новости</a></div>
+<div id="menu"><a href="*">Работы</a></div>
+<div id="menu"><a href="*">Администрация</a></div>
+<div id="menu"><a href="*">О нас</a></div>
+<div id="menu"><a href="*">Авторизация/Регистация</a></div>
+
